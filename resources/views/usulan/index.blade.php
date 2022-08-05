3030203226331389d66c7cd5c8005d00c413224d
Muhammad Davi <davi_muhammad@yahoo.com>
2022-08-05 16:59:55 +0700

    fix error usulan index

--- resources/views/usulan/index.blade.php
+++ resources/views/usulan/index.blade.php
@@ -32,15 +32,9 @@
                             <span class="text-lg font-bold ml-3 my-auto">{{ session('error') }}</span>
                         </div>
                     @endif
-                    @hasrole('Admin')
-                        @if($kouta->sisa < 1)
-                        <a href="#"
-                           class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded m-2 float-right">Kouta formasi perpanjangan telah penuh!</a>
-                        @else
-                        <a href="{{ route('usulan.create') }}"
-                           class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2 float-right">Buat Usulan</a>
-                        @endif
-                    @endhasrole
+                    <a href="{{ route('usulan.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2 float-right">
+                        Buat Usulan
+                    </a>
 
                     <table class="table-fixed w-full mt-4">
                         <thead class="bg-gray-700">
